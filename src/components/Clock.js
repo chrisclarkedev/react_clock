e3770ffccf025db8f96b1d1cb70607d3deb4e798
chrisclarkedev <cclarke8904@gmail.com>
2021-09-04 11:46:28 -0400

    added structure of clock component

---
+++ src/components/Clock.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+function Clock() {
+  return (
+    <div className="Clock">
+      {/* Hand that shows hour */}
+      <div className="hand hour"></div>
+
+      {/* Hand that shows minute */}
+      <div className="hand minute"></div>
+
+      {/* Hand that shows seconds */}
+      <div className="hand second"></div>
+
+      {/* Each hour of the clock in # */}
+      <div className="number number1">
+        <div>1</div>
+      </div>
+      <div className="number number2">
+        <div>2</div>
+      </div>
+      <div className="number number3">
+        <div>3</div>
+      </div>
+      <div className="number number4">
+        <div>4</div>
+      </div>
+      <div className="number number5">
+        <div>5</div>
+      </div>
+      <div className="number number6">
+        <div>6</div>
+      </div>
+      <div className="number number7">
+        <div>7</div>
+      </div>
+      <div className="number number8">
+        <div>8</div>
+      </div>
+      <div className="number number9">
+        <div>9</div>
+      </div>
+      <div className="number number10">
+        <div>10</div>
+      </div>
+      <div className="number number11">
+        <div>11</div>
+      </div>
+      <div className="number number12">
+        <div>12</div>
+      </div>
+    </div>
+  );
+}
+
+export default Clock;
