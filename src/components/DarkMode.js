679682c7b1f789322c9d1c1e322bc579daf37954
chrisclarkedev <cclarke8904@gmail.com>
2021-09-09 13:12:47 -0400

    function for switching themes, local stogage usage, click event for button

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -8,7 +8,32 @@ const DarkMode = () => {
   const darkTheme = 'dark';
   let theme;
 
-  return <button>Click Me</button>;
+  // Setting value for either light or dark in local storage
+  if (localStorage) {
+    theme = localStorage.getq('theme');
+  }
+  // Whichever theme is chosen would be apply the css class to the body
+  if (theme === lightTheme || theme === darkTheme) {
+    body.classList.add(theme);
+  } else {
+    // If no theme is chosen by default the light class will be applied
+    body.classLight.add(lightTheme);
+  }
+
+  // Checks which them is currently active
+  const switchTheme = (e) => {
+    if (theme === darkTheme) {
+      body.classLight.replace(darkTheme, lightTheme);
+    }
+  };
+
+  return (
+    <button
+      className={theme === 'dark' ? clickedClass : ''}
+      id="darkMode"
+      onClick={(e) => switchTheme(e)}
+    ></button>
+  );
 };
 
 export default DarkMode;
