f471c2ed18c0a4fb5d9e0d43fc3abd4c37d5e8f5
chrisclarkedev <cclarke8904@gmail.com>
2021-09-09 13:02:47 -0400

    dark mode component create, stylesheet created

---
+++ src/components/DarkMode.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/DarkMode.css';
+
+const DarkMode = () => {
+  let clickedClass = 'clicked';
+  const body = document.body;
+  const lightTheme = 'light';
+  const darkTheme = 'dark';
+  let theme;
+
+  return <button>Click Me</button>;
+};
+
+export default DarkMode;
